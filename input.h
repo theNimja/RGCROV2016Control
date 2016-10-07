1924757843da99c1893671cd79af7a9b0d031d4a
nimrod libman <nimrodlibman@yahoo.c.uk>
2016-10-07 18:12:38 +0100

    We arent working on wiichuck anymore, abondoning branch

--- input.h
+++ input.h
@@ -1,11 +1,38 @@
+#include "nunchuck_funcs.h";
 
+const float deadZoneBuffer = 0.1;
 
 static void setupFunctions(){
-  
+
+  //set up the powerpins and the loop for getting data
+  nunchuck_setpowerpins();
+  nunchuck_init();
   
   }
 
 static void input(){
+  //decode input to data usable by the control program
+  nunchuck_get_data();
+  int xPushRaw = nunchuck_joyx();
+  int yPushRaw = nunchuck_joyy();
+  bool zButton = nunchuck_zbutton();
+  bool cButton = nunchuck_cbutton();
+
+  float xPush = (xPush/255) -0.5;
+  float yPush = (yPush/255) -0.5;
+  //No buttons = lateral
+  if (abs(xPush) > abs(yPush)){
+    //forwards/backwards
+    if (xpush>deadZoneBuffer){
+      
+      }
+    
+    }
+  
+
+  //z-pitch/roll
+  
+  //c-vertical/yaw
   
   
   }
